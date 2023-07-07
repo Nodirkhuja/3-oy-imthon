61d42f4a9ac3986c6e46e235d44f6cac046bb0aa
Nodirkhuja <fayzullayevnodirxoja@gmail.com>
2023-07-07 23:52:48 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+var elChangeThemeSiteBtn = document.querySelector(".theme-btn");
+
+elChangeThemeSiteBtn.addEventListener("click", function(){
+  document.body.classList.toggle("dark");
+})
+
+
+$(document).ready(function(){
+  $('.customer-list').slick({
+    
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
